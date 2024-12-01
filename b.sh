7142f2d5e1750aced11081bccfb43c028e287991
jaz <lordxarus@gmail.com>
2024-12-01 13:16:11 -0500

    added time information to build script

--- b.sh
+++ b.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo building @ $(date +%H:%M)
 gcc -Wall -Wextra -O2 -g server.c common.c -o server
 gcc -Wall -Wextra -O2 -g client.c common.c -o client
+echo "============================================================================="
